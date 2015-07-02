99e2516681394e2ae5d94ababe6cb5bf0f865bbb
larvata <alzzlnewtype@gmail.com>
2015-07-02 17:35:59 +0800

    clear start.sj

--- src/start.js
+++ src/start.js
@@ -1,12 +1,3 @@
 require('babel/register');
 
 module.exports = require('./server/server');
-
-// import miki from './assistance/miki';
-// // import server from './server/server';
-// import Tashima from './assistance/meru';
-// meru = new Tashima(miki);
-
-// meru.startService();
-
-// server.start();
